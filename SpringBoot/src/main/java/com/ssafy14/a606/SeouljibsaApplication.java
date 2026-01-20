4b02561e4e7207720647c72e576d041e2d315621
mingy1206 <mingy837@gmail.com>
2026-01-20 14:07:45 +0900

    Init: 프로젝트 구조 setup

---
+++ SpringBoot/src/main/java/com/ssafy14/a606/SeouljibsaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy14.a606;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeouljibsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeouljibsaApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/src/test/java/com/ssafy14/a606/SeouljibsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy14.a606;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeouljibsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
