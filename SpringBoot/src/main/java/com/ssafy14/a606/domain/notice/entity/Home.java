5afd201441e2991cc257c1db31ac37d5074cd099
wjsrlwjd <wjsrlwjd@kookmin.ac.kr>
2026-01-30 10:37:42 +0900

    Feat: fastAPI 질문필터링 추가, 프롬프트 개선

--- SpringBoot/src/main/java/com/ssafy14/a606/domain/notice/entity/Home.java
+++ SpringBoot/src/main/java/com/ssafy14/a606/domain/notice/entity/Home.java
@@ -22,6 +22,4 @@ public class Home {
     @JoinColumn(name = "notice_id") // notice_id 컬럼으로 Notice와 조인
     private Notice notice;
 
-    // 다른 필드들은 Home 도메인 담당자가 최종적으로 추가할 예정입니다.
-    // Notice 와의 관계 설정을 위해 임시로 생성된 파일입니다.
 }
