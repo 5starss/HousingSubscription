d4e0b47816ffdcc162eb8f0e3482701b2e786fd0
chowh0823 <ccc2900@naver.com>
2026-01-30 10:43:11 +0900

    Feat: 기본 정보 조회 & 수정

--- Front/src/api/UserApi.ts
+++ Front/src/api/UserApi.ts
@@ -1,11 +1,35 @@
 import { apiClient } from "./axiosConfig";
 import { type UserAddInfo } from "../types/user";
 
+// 기본 정보 조회 (GET)
+export const getUserBasicInfo = async (): Promise<{ userName: string; email: string; loginId: string }> => {
+  try{
+    const response = await apiClient.get("/users/me");
+    return response.data;
+  } catch (error: unknown) {
+    // 에러 처리
+    console.error("기본 정보 조회 실패:", error);
+    throw error;
+  }
+};
+
+// 기본 정보 수정 (PATCH)
+export const updateUserBasicInfo = async (data: { userName: string; email: string }) => {
+  try{
+    const response = await apiClient.patch("/users/me", data);
+    return response.data;
+  } catch (error: unknown) {
+    // 에러 처리
+    console.error("기본 정보 수정 실패:", error);
+    throw error;
+  }
+};
+
 // 추가 정보 조회 (GET)
 export const getUserAddInfo = async (): Promise<UserAddInfo> => {
   try{
     const response = await apiClient.get("/users/me/info");
-  return response.data;
+    return response.data;
   } catch (error: unknown) {
     // 에러 처리
     console.error("추가 정보 조회 실패:", error);
